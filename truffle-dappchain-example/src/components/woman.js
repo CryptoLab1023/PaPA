60d1c2a96c0b67c288cf79c93357702c87aa0b36
オムラユウキ <>
2018-08-11 13:28:49 +0900

    front

---
+++ truffle-dappchain-example/src/components/woman.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react' ;
+
+
+export default class Woman extends Component {
+
+    render() {
+        return (
+        <div class="select-woman">
+                <div class="select-img">
+                    <img src="../img/www.pakutaso.com-shared-img-thumb-SAYA160312500I9A3721-400x287.jpg" alt="" />
+                    <div class="select-age-message">
+                        <div class="select-info">
+                            <span>25才</span>
+                            <span>東京都</span>
+                        </div>
+                        <div class="select-message-container">
+                            <span class="select-message">message</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        )
+    }
+}
