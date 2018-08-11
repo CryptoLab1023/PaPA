f3a752a7f221d51a3f167619cb82cc2f61a65ecc
CryptoLab1023 <allscience1023@gmail.com>
2018-08-11 16:04:33 +0900

    json-serve work

--- truffle-dappchain-example/src/components/woman.js
+++ truffle-dappchain-example/src/components/woman.js
@@ -4,21 +4,26 @@ import React, {Component} from 'react' ;
 export default class Woman extends React.Component {
 
     render() {
+
+        const style = {
+            "backgroundColor": "blue",
+        }
+
         return (
-        <div class="select-woman">
-                <div class="select-img">
-                    <img src="../img/www.pakutaso.com-shared-img-thumb-SAYA160312500I9A3721-400x287.jpg" alt="" />
-                    <div class="select-age-message">
-                        <div class="select-info">
-                            <span>25才</span>
-                            <span>東京都</span>
-                        </div>
-                        <div class="select-message-container">
-                            <span class="select-message">message</span>
+            <div className = "select-woman" style = {style} >
+                    <div className="select-img">
+                        <img src="../img/www.pakutaso.com-shared-img-thumb-SAYA160312500I9A3721-400x287.jpg" alt="" />
+                        <div className="select-age-message">
+                            <div className="select-info">
+                                <span>25才</span>
+                                <span>東京都</span>
+                            </div>
+                            <div className="select-message-container">
+                                <span className="select-message">message</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-        </div>
+            </div>
         )
     }
 }
--- truffle-dappchain-example/src/index.js
+++ truffle-dappchain-example/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import http from 'http'
 import Contract from './contract'
 import Home from './components/home'
 
 const Index = class Index extends React.Component {
+
   constructor(props) {
     super(props)
 
@@ -17,11 +19,40 @@ const Index = class Index extends React.Component {
     }
   }
 
+
   async componentWillMount() {
+
+    const Request = async (object) => {
+      if (object.method == "GET")
+        return res = await http.get({
+          hostname: object.hostname,
+          port: object.port,
+          path: object.path,
+          agent: false // create a new agent just for this one request
+        })
+      else if (object.method == "POST")
+        return res = await http.post({
+          hostname: object.hostname,
+          port: object.port,
+          path: object.path,
+          agent: false // create a new agent just for this one request
+        })
+    }
+
     await this.contract.loadContract()
     this.contract.addEventListener((v) => {
       this.setState({ value: v._value })
     })
+    const object = {
+      hostname: "localhost",
+      port: "8000",
+      path: "/posts",
+      method: "GET"
+    }
+    const responseData = await Request(object)
+    console.log(responseData)
+    this.setState({responseData})
+
   }
 
   onChangeHandler(event) {
